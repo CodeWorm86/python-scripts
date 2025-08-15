a2d83854bb3bb3684e6dd0c5835e5b34ec922778
codeWorm86 <42699908+CodeWorm86@users.noreply.github.com>
2025-08-15 10:57:17 +0300

    Basic Arithmetic

---
+++ basic_arithmetic_operations.py
@@ -0,0 +1,41 @@
+# Six Basic Arithmetic Operations 
+
+
+print(" ")
+print("Basic Arithmetic Operations")
+print("---------------------------")
+
+# Addition +
+add = 2+2
+add_string = "2 + 2 = "
+print(add_string, add)
+
+# Subtraction -
+
+subtract = 100-50
+subtract_str = "100 - 50 = "
+print(subtract_str, subtract)
+
+# Multiplication * 
+
+multiply = 35 * 35
+multiply_str = "35 * 35 = "
+print(multiply_str, multiply)
+
+# Division /
+
+divide = 1000 / 100
+divide_str = "1000 / 100 = "
+print(divide_str, divide)
+
+# Modulo (remainder) %
+
+modulo = 7 % 3
+modulo_str = "7 % 3 = "
+print(modulo_str,modulo)
+
+# Exponetiation **
+
+exponent = 2**3
+exponent_str = "2^3 = "
+print(exponent_str, exponent)
